0ba246947f2c4f973d29087eba779029cc1f4957
Алексей <agragregra@ya.ru>
2015-12-08 10:27:21 +0700

    Commit

--- script/mail.php
+++ script/mail.php
@@ -2,33 +2,42 @@
 
 $method = $_SERVER['REQUEST_METHOD'];
 
-//Script Settings
-$frm_name  = "Project Name";     //Change
-$recepient = "agragregra@ya.ru"; //Change
-$subject   = "E-mail Subject";   //Change
-
 //Script Foreach
 $c = true;
 if ( $method === 'POST' ) {
+
+	$project_name = trim($_POST["project_name"]);
+	$admin_email  = trim($_POST["admin_email"]);
+	$form_subject = trim($_POST["form_subject"]);
+
 	foreach ( $_POST as $key => $value ) {
-		$message .= "
+		if ( $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+			$message .= "
 			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
 				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
 				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
 			</tr>
-		";
+			";
+		};
 	};
 } else if ( $method === 'GET' ) {
+
+	$project_name = trim($_GET["project_name"]);
+	$admin_email  = trim($_GET["admin_email"]);
+	$form_subject = trim($_GET["form_subject"]);
+
 	foreach ( $_GET as $key => $value ) {
-		$message .= "
-			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			</tr>
-		";
+		if ( $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+			$message .= "
+				" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+					<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+					<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+				</tr>
+			";
+		};
 	};
 };
 
 $message = "<table style='width: 100%;'>$message</table>";
 
-mail($recepient, $subject, $message, "From: $frm_name <$recepient>" . "\r\n" . "Reply-To: $recepient" . "\r\n" . "X-Mailer: PHP/" . phpversion() . "\r\n" . "Content-type: text/html; charset=\"utf-8\"");
+mail($admin_email, $form_subject, $message, "From: $project_name <$admin_email>" . "\r\n" . "Reply-To: $admin_email" . "\r\n" . "X-Mailer: PHP/" . phpversion() . "\r\n" . "Content-type: text/html; charset=\"utf-8\"");
