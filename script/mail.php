e2b07df980401060cd4d8507d8bad73ef0adad52
Алексей <agragregra@ya.ru>
2015-12-08 10:37:16 +0700

    Commit

--- script/mail.php
+++ script/mail.php
@@ -11,15 +11,15 @@ if ( $method === 'POST' ) {
 	$form_subject = trim($_POST["form_subject"]);
 
 	foreach ( $_POST as $key => $value ) {
-		if ( $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
 			$message .= "
 			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-				<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-			</tr>
-			";
-		};
+			<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+			<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+		</tr>
+		";
 	};
+};
 } else if ( $method === 'GET' ) {
 
 	$project_name = trim($_GET["project_name"]);
@@ -27,15 +27,15 @@ if ( $method === 'POST' ) {
 	$form_subject = trim($_GET["form_subject"]);
 
 	foreach ( $_GET as $key => $value ) {
-		if ( $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
+		if ( $value != "" && $key != "project_name" && $key != "admin_email" && $key != "form_subject" ) {
 			$message .= "
-				" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
-					<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
-					<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
-				</tr>
-			";
-		};
+			" . ( ($c = !$c) ? '<tr>':'<tr style="background-color: #f8f8f8;">' ) . "
+			<td style='padding: 10px; border: #e9e9e9 1px solid;'><b>$key</b></td>
+			<td style='padding: 10px; border: #e9e9e9 1px solid;'>$value</td>
+		</tr>
+		";
 	};
+};
 };
 
 $message = "<table style='width: 100%;'>$message</table>";
