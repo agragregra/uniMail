6877e2b768e552084efe4e1efeebeb32d2c141f2
Алексей <agragregra@ya.ru>
2024-01-10 18:46:24 +0300

    Update

--- readme.md
+++ readme.md
@@ -3,11 +3,9 @@
 <h3>How to use uniMail</h3>
 
 <ol>
-	<li>Include jQuery</li>
-	<li>Include Ajax code from script.js</li>
-	<li>Change Ajax selector in you JS</li>
-	<li>Create HTML form with you form class</li>
-	<li>Create HTML Hidden Required Fields with you values</li>
-	<li>Change path to mail.php in you JS</li>
+	<li>Connect <code>script.js</code> file to your project and Specify location <code>mailPath</code> in this file</li>
+	<li>Add <code>uniForm</code> class to the desired forms tags in HTML (like index.html example)</li>
+	<li>Add <strong>Hidden Required Fields</strong> to your forms in HTML (like index.html example)</li>
+	<li>Add any set of fields to your forms in HTML (like index.html example)</li>
 	<li>Done!</li>
 </ol>
--- script/script.js
+++ script/script.js
@@ -1,20 +1,25 @@
-$(document).ready(function() {
+const mailPath = './mail.php'
 
-	// E-mail Ajax Send
-	$("form").submit(function() { // Change
-		var th = $(this);
-		$.ajax({
-			type: "POST",
-			url: "mail.php", // Change
-			data: th.serialize()
-		}).done(function() {
-			alert("Thank you!");
-			setTimeout(function() {
-				// Done Functions
-				th.trigger("reset");
-			}, 1000);
-		});
-		return false;
-	});
+document.querySelectorAll('.uniForm').forEach( (e) => {
 
-});
+	e.addEventListener('submit', function(e) {
+
+			let th      = this,
+					params  = new FormData(this),
+					request = new XMLHttpRequest()
+
+			request.open('POST', mailPath, true)
+			request.send(params)
+
+			request.onreadystatechange = function() {
+					if (this.readyState == 4 && this.status == 200) {
+							setTimeout(function() { th.reset() }, 1000)
+							alert('Thank you!')
+					}
+			}
+
+			e.preventDefault()
+
+	})
+
+})
